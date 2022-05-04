81ae0943841851e0c7f0cdd9fa0fc3c8eb4c17c5
Natalia_Markina <natalia.marckina2010@yandex.ru>
2022-05-04 14:38:55 +0300

    Create project

---
+++ TestTaskMovies/TestTaskMovies/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskMovies
+//
+//  Created by Natalia on 04.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
