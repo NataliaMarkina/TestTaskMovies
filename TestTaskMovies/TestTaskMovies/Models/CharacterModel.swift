d74ff7ee27edbce824c99900e42d7090fabb1f29
Natalia_Markina <natalia.marckina2010@yandex.ru>
2022-05-14 07:36:40 +0300

    Add CharacterModel

---
+++ TestTaskMovies/TestTaskMovies/Models/CharacterModel.swift
@@ -0,0 +1,44 @@
+//
+//  CharacterModel.swift
+//  TestTaskMovies
+//
+//  Created by Natalia on 08.05.2022.
+//
+
+struct CharacterModel: Codable {
+    var name: String?
+    var height: String?
+    var mass: String?
+    var hairColor: String?
+    var skinColor: String?
+    var eyeColor: String?
+    var birthYear: String?
+    var gender: String?
+    var homeworld: String?
+    var films: [String]?
+    var species: [String]?
+    var vehicles: [String]?
+    var starships: [String]?
+    var created: String?
+    var edited: String?
+    var url: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case height
+        case mass
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender
+        case homeworld
+        case films
+        case species
+        case vehicles
+        case starships
+        case created
+        case edited
+        case url
+    }
+}
